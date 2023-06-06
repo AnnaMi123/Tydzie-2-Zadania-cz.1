ed0357829a8878cd46991a49a21de461bbd2abe7
Migaś <Miga@DESKTOP-0840BS7>
2023-06-06 16:17:50 +0200

    Add project files.

---
+++ Tydzień 2 Zadania cz.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Sławomir";
+Console.WriteLine(name);
+string surname = "Kwiatkowski";
+Console.WriteLine (surname);
+int age = 30;
+Console.WriteLine(age);
+char gender = 'm';
+Console.WriteLine(gender);
+long pesel = 93110278177;
+Console.WriteLine(pesel);
+long ID = 23456789;
+Console.WriteLine(ID);
